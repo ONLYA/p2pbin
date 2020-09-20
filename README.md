0e90f676c42972914e10a8c1a4ccdf17b1c4e393
Glitch (hello-express) <none>
2020-09-20 21:30:14 +0000

    🤘🌝 Checkpoint ./index.html:8045597/10400 ./main.js:8045597/1442 ./README.md:8045597/432 ./.gitignore:8045597/4

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
-# ipfsbin
+# IPFSbin
+***
+A pastebin based on IPFS. It is a 100% static website running on JavaScript and HTML.
 
-Pastebin, for IPFS
+Basically:
+1. You create a paste
+2. You get the hash
+3. You pin it. The save modal give you instructions on how to pin it.
+
+To be added: 
+- A simple button to pin directly to [Pinata](https://pinata.cloud) with it's API.
+
+Project URL: https://ipfsbin.glitch.me/
+
+Lol I accidently put it in #glitch-help
--- main.js
+++ main.js
@@ -1,22 +1,20 @@
 document.addEventListener("DOMContentLoaded", async () => {
   const node = await Ipfs.create({ repo: "ipfs-" + Math.random() });
-        window.node = node;
+  window.node = node;
 
-        const status = node.isOnline() ? "&nbsp;⬤ (Ready!)" : "&nbsp;⭘ (Not Ready!)";
+  const status = node.isOnline() ? "&nbsp;⬤ (Ready!)" : "&nbsp;⭘ (Not Ready!)";
 
-        console.log(`Node status: ${status}`);
-        document.getElementById("status").innerHTML = `IPFS status: ${status}`;
-var params = new URLSearchParams(document.location.search.substring(1));
-var id = params.get("id");
-if (!id){
-  id = "QmZbCgzQUKensoruM7ghihFBD5vLp8g15CSrzTxyRpd2Mi"
-}
-        
-      for await (const data of
-      node.cat(id)) {
-    document.getElementById("body").value = filterXSS(data.toString()) } 
+  console.log(`Node status: ${status}`);
+  document.getElementById("status").innerHTML = `IPFS status: ${status}`;
+  var params = new URLSearchParams(document.location.search.substring(1));
+  var id = params.get("id");
+  if (!id) {
+    id = "QmZbCgzQUKensoruM7ghihFBD5vLp8g15CSrzTxyRpd2Mi";
+  }
+
+  for await (const data of node.cat(id)) {
+    document.getElementById("body").value = filterXSS(data.toString());
+  }
   body.disabled = false;
-   save.disabled = false;
-  
-
-      });
+  save.disabled = false;
+});
