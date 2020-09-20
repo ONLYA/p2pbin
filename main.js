0c19b01c0feb088598f7c45e27247eddc7d8628a
Glitch (hello-express) <none>
2020-09-20 16:44:30 +0000

    🐄🚣 Checkpoint ./index.html:8045597/745 ./style.css:8045597/144 ./test.html:8045597/7634 ./main.js:8045597/661

---
+++ main.js
@@ -0,0 +1,14 @@
+
+      document.addEventListener("DOMContentLoaded", async () => {
+        const node = await Ipfs.create({ repo: "ipfs-" + Math.random() });
+        window.node = node;
+
+        const status = node.isOnline() ? "⬤" : "⭘";
+
+        console.log(`Node status: ${status}`);
+        document.getElementById("status").innerHTML = `Node status: ${status}`;
+
+        // You can write more code here to use it. Use methods like
+        // node.add, node.get. See the API docs here:
+        // https://github.com/ipfs/js-ipfs/tree/master/packages/interface-ipfs-core
+      });
