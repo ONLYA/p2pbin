a0c49e9a5423ffca0f3df4bb776ec3237f9dda4f
Glitch (hello-express) <none>
2020-09-20 21:18:38 +0000

    🍦🙆 Checkpoint ./index.html:8045597/9748 ./main.js:8045597/106

--- main.js
+++ main.js
@@ -2,14 +2,14 @@ document.addEventListener("DOMContentLoaded", async () => {
   const node = await Ipfs.create({ repo: "ipfs-" + Math.random() });
         window.node = node;
 
-        const status = node.isOnline() ? "⬤ (Ready!)" : "⭘ (Not Ready!)";
+        const status = node.isOnline() ? "&nbsp;⬤ (Ready!)" : "&nbsp;⭘ (Not Ready!)";
 
         console.log(`Node status: ${status}`);
         document.getElementById("status").innerHTML = `IPFS status: ${status}`;
 var params = new URLSearchParams(document.location.search.substring(1));
 var id = params.get("id");
 if (!id){
-  id = "QmQzCQn4puG4qu8PVysxZmscmQ5vT1ZXpqo7f58Uh9QfyY"
+  id = "QmZbCgzQUKensoruM7ghihFBD5vLp8g15CSrzTxyRpd2Mi"
 }
         
       for await (const data of
