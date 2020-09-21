2e093982e03011d9d0fa73953243a224f343f22e
Glitch (ipfsbin) <none>
2020-09-21 15:50:18 +0000

    🌼💪 Checkpoint ./index.html:8045597/12 ./raw/index.html:8045597/1831 ./main.js:8045597/106

--- main.js
+++ main.js
@@ -25,6 +25,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 
   for await (const data of node.cat(id)) {
     document.getElementById("body").value = filterXSS(data.toString());
+    document.getElementById("body").innerHTML = filterXSS(data.toString()).relpace("\n", "");
   }
   body.disabled = false;
   save.disabled = false;
